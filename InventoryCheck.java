f877aabd40460724dd5c7edd460dce7ff525c406
jbruhl55 <jeremybruhl55@gmail.com>
2017-04-20 11:11:53 -0500

    Update InventoryCheck.java

--- InventoryCheck.java
+++ InventoryCheck.java
@@ -7,54 +7,78 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton; 
 import javax.swing.JFrame; 
 import javax.swing.JLabel;
-public class InventoryCheck extends JFrame implements ActionListener
-{
-    ArrayList<InventoryItem> Inventory = new ArrayList();
-    public InventoryDemo()
-    {
-        JButton addDirectory = new JButton("Add Directory");
-        JButton removeDirectory = new JButton("Remove Directory");
-        JButton addItem = new JButton("Add Item");
-        JButton removeItem = new JButton("Remove Item");
-        JButton setMinimum = new JButton("Set Minimum");
-        JButton order = new JButton("Order");
-        JFrame f = new JFrame("InventoryCheck");
-        f.setLayout(new FlowLayout());
-        f.setSize(1000,500);
-        f.add(addDirectory);
-        f.add(removeDirectory);
-        f.add(addItem);
-        f.add(removeItem);
-        f.add(setMinimum);
-        f.add(order);
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        f.setVisible(true);
-        addDirectory.addActionListener(this);
-        setVisible(true);
-    } 
-    private static void createGUI()
-    {
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("Inventory Demo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        InventoryCheck pane = new InventoryCheck();
-        frame.getRootPane().setDefaultButton();
-        
-    }
-    public static void main(String[] args) 
-    {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() 
-        {
-                public void run()
-                {
-                    createGUI();
-                }
-        }
-        );
+public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new InventoryCheck().createGui();
+            }
+        });
+
     }
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void createGui() {
+        JFrame frame = new JFrame("InventoryCheck");
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel(new GridBagLayout());
+        frame.add(panel);
+        frame.getContentPane().add(panel, BorderLayout.WEST);
+        GridBagConstraints c = new GridBagConstraints();
+
+        JButton button1 = new JButton("Add Directory ");
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(40, 40, 40, 40);
+        panel.add(button1, c);
+        button1.addActionListener(this);
+
+        JButton button2 = new JButton("Remove Directory");
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(button2, c);
+        button2.addActionListener(this);
+
+        JButton button3 = new JButton("Add Item");
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(button3, c);
+        button3.addActionListener(this);
+
+        JButton button4 = new JButton("Remove Item");
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(button4, c);
+        button4.addActionListener(this);
+
+        JButton button5 = new JButton("Set Minimum");
+        c.gridx = 0;
+        c.gridy = 4;
+        panel.add(button5, c);
+        button5.addActionListener(this);
+        
+        JButton button6 = new JButton("Order");
+        c.gridx = 0;
+        c.gridy = 5;
+        panel.add(button6, c);
+        button6.addActionListener(this);
+        
+        JButton button7 = new JButton("Search");
+        c.gridx = 0;
+        c.gridy = 6;
+        panel.add(button7, c);
+        button7.addActionListener(this);
+    }
+    
+
+    public void actionPerformed(ActionEvent e) {
+        JFrame frame2 = new JFrame("InventoryCheck");
+        frame2.setVisible(true);
+        frame2.setSize(600, 600);
+        JLabel label = new JLabel("InventoryCheck");
+        JPanel panel = new JPanel();
+        frame2.add(panel);
+        panel.add(label);
     }
 }
