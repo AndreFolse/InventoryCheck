26572d36a48a25c44ed496cc1f00aeacf6105a8c
jbruhl55 <jeremybruhl55@gmail.com>
2017-04-20 13:11:32 -0500

    Update InventoryCheck.java

--- InventoryCheck.java
+++ InventoryCheck.java
@@ -1,13 +1,18 @@
 package inventorycheck;
-import java.util.*;
-import java.awt.EventQueue; 
-import java.awt.FlowLayout; 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton; 
-import javax.swing.JFrame; 
-import javax.swing.JLabel;
-public static void main(String[] args) {
+public class InventoryCheck implements ActionListener {
+    JButton button1 = new JButton("Add Directory ");
+    JButton button2 = new JButton("Remove Directory ");
+    JButton button3 = new JButton("Add Item");
+    JButton button4 = new JButton("Remove Item");
+    JButton button5 = new JButton("Order");
+    JButton button6 = new JButton("Search");
+    
+
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -19,7 +24,7 @@ public static void main(String[] args) {
 
     public void createGui() {
         JFrame frame = new JFrame("InventoryCheck");
-        frame.setSize(1000, 1000);
+        frame.setSize(300, 350);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel(new GridBagLayout());
@@ -27,58 +32,93 @@ public static void main(String[] args) {
         frame.getContentPane().add(panel, BorderLayout.WEST);
         GridBagConstraints c = new GridBagConstraints();
 
-        JButton button1 = new JButton("Add Directory ");
+        
         c.gridx = 0;
         c.gridy = 0;
-        c.insets = new Insets(40, 40, 40, 40);
+        c.insets = new Insets(10, 10, 10, 10);
         panel.add(button1, c);
         button1.addActionListener(this);
 
-        JButton button2 = new JButton("Remove Directory");
+       
         c.gridx = 0;
         c.gridy = 1;
         panel.add(button2, c);
         button2.addActionListener(this);
 
-        JButton button3 = new JButton("Add Item");
+        
         c.gridx = 0;
         c.gridy = 2;
         panel.add(button3, c);
         button3.addActionListener(this);
 
-        JButton button4 = new JButton("Remove Item");
+        
         c.gridx = 0;
         c.gridy = 3;
         panel.add(button4, c);
         button4.addActionListener(this);
-
-        JButton button5 = new JButton("Set Minimum");
+        
+        
         c.gridx = 0;
         c.gridy = 4;
         panel.add(button5, c);
         button5.addActionListener(this);
         
-        JButton button6 = new JButton("Order");
+        
         c.gridx = 0;
         c.gridy = 5;
         panel.add(button6, c);
         button6.addActionListener(this);
-        
-        JButton button7 = new JButton("Search");
-        c.gridx = 0;
-        c.gridy = 6;
-        panel.add(button7, c);
-        button7.addActionListener(this);
     }
     
 
+    @Override
     public void actionPerformed(ActionEvent e) {
         JFrame frame2 = new JFrame("InventoryCheck");
+        JFrame frame3 = new JFrame("InventoryCheck");
+        JFrame frame4 = new JFrame("InventoryCheck");
+        JFrame frame5 = new JFrame("InventoryCheck");
+        JFrame frame6 = new JFrame("InventoryCheck");
+        JFrame frame7 = new JFrame("InventoryCheck");
+        JLabel label1 = new JLabel("Add Directory");
+        JLabel label2 = new JLabel("Remove Directory");
+        JLabel label3 = new JLabel("Add Item");
+        JLabel label4 = new JLabel("Remove Item");
+        JLabel label5 = new JLabel("Order");
+        JLabel label6 = new JLabel("Search");
         frame2.setVisible(true);
         frame2.setSize(600, 600);
-        JLabel label = new JLabel("InventoryCheck");
         JPanel panel = new JPanel();
-        frame2.add(panel);
-        panel.add(label);
+        if(button1.isEnabled())
+        {
+            frame2.add(panel);
+            panel.add(label1);
+        }
+        else if(button2.isEnabled())
+        {
+            JPanel panel2 = new JPanel();
+            frame3.add(panel2);
+            panel2.add(label2);
+        }
+        else if(button3.isEnabled())
+        {
+            frame4.add(panel);
+            panel.add(label3);
+        }
+        else if(button4.isEnabled())
+        {
+            frame5.add(panel);
+            panel.add(label4);
+        }
+        else if(button5.isEnabled())
+        {
+            frame6.add(panel);
+            panel.add(label5);
+        }
+        else
+        {
+            frame7.add(panel);
+            panel.add(label6);
+        }
+        
     }
 }
